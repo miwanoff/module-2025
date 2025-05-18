99d05a0b0b17f5bd8d501917b2912aa391e3288c
miwanoff <marina.iva.noffa@gmail.com>
2025-05-18 15:11:09 +0300

    Add fhello unction

--- login.js
+++ login.js
@@ -1,6 +1,10 @@
 import {User, Student} from "./user.js";
+import {hello} from "./hello.js";
+
 let user = new User("John");
 console.log(user.name);
 
 let student = new Student("Joan", "KhPI");
 console.log(student.name, student.university);
+
+hello(student.name)
